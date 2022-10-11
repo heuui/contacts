d4bc833111d7d8ffe967ebcadd677e4589625866
heuui <henni.haavistola@gmail.com>
2022-10-11 16:01:42 +0300

    hakee yhteystietojen nimet+numerot ja näyttää ne murtosekunin ajan ennen virheilmoitusta

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import * as Contacts from'expo-contacts';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [contact, setContact] = useState({});
+
+
+  const getContacts = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status === 'granted') {
+      const { data } = await Contacts.getContactsAsync(
+        { fields: [Contacts.Fields.PhoneNumbers], }
+        ); 
+      if (data.length > 0) {
+        setContact(data); 
+      }
+    }
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+        <FlatList 
+          data={contact} 
+          renderItem={({item}) =>
+          <View style={styles.flatlist}>
+            <Text>{item.name} {item.phoneNumbers[0].number}</Text>
+          </View>
+        } />
+        <Button 
+          title="Get contacts" 
+          onPress={getContacts} 
+        />
+    </SafeAreaView>
   );
 }
 
@@ -17,4 +44,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  flatlist: {
+    flexDirection: 'row',
+    marginTop: 10,
+   },
 });
